f123488da004553c198984aad3fe92a2e5079b60
Swaiku <prin.jeremy@protonmail.ch>
2026-03-06 09:47:21 +0100

    🔖 Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 project = "EBNF Compiler"
 copyright = "2026, Jérémy Prin"
 author = "Jérémy Prin"
-release = "0.2.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/ebnf_compiler/__init__.py
+++ src/ebnf_compiler/__init__.py
@@ -25,7 +25,7 @@ from .scanner import Scanner
 console = Console()
 app = typer.Typer()
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 @dataclass
